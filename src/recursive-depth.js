d2cb16929703ff7219b9f063a3e32890c03c8c06
Maxim Kirichenko <kirichenkomaxim@icloud.com>
2023-04-11 19:11:32 +0300

    feat: add solution: recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,10 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  calculateDepth(arr) {
+    let count = 0;
+
+    if (!Array.isArray(arr)) {
+      return 0;
+    }
+
+    arr.forEach(element => {
+      let height = this.calculateDepth(element);
+
+      if (height > count) {
+        count = height
+      };
+    });
+
+    return count + 1;
+  };
 }
 
 module.exports = {
