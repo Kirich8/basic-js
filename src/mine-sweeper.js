29c22da3e1db4f49f11103cf592c34ddf66c8467
Maxim Kirichenko <kirichenkomaxim@icloud.com>
2023-04-11 18:01:54 +0300

    feat: add solution: mine

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const matrix1 = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix1.push([]);
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+
+      if (matrix[i][j] === true) {
+        if (i == 0) {
+          matrix1[i + 1][j] = matrix1[i + 1][j] + 1 || 1;
+        }
+        if (i > 0 && i < matrix.length - 1) {
+          matrix1[i + 1][j] = matrix1[i + 1][j] + 1 || 1;
+          matrix1[i - 1][j] = matrix1[i - 1][j] + 1 || 1;
+        }
+        if (j > 0 && j < matrix[i].length - 1) {
+          matrix1[i][j + 1] = matrix1[i][j + 1] + 1 || 1;
+          matrix1[i][j - 1] = matrix1[i][j - 1] + 1 || 1;
+        }
+        if (j == 0) {
+          matrix1[i][j + 1] = matrix1[i][j + 1] + 1 || 1;
+        }
+        if (i == 0 && j == 0) {
+          matrix1[i + 1][j + 1] = matrix1[i + 1][j + 1] + 1 || 1;
+        }
+        if (i > 0 && i < matrix.length - 1 && j > 0 && j < matrix[i].length - 1) {
+          matrix1[i - 1][j - 1] = matrix1[i - 1][j - 1] + 1 || 1;
+          matrix1[i + 1][j + 1] = matrix1[i + 1][j + 1] + 1 || 1;
+          matrix1[i - 1][j + 1] = matrix1[i - 1][j + 1] + 1 || 1;
+          matrix1[i + 1][j - 1] = matrix1[i + 1][j - 1] + 1 || 1;
+        }
+      };
+      if (matrix[i][j] === false) {
+        matrix1[i][j] = matrix1[i][j] || 0;
+      }
+    }
+  }
+
+  return matrix1;
 }
 
 module.exports = {
